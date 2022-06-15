270ea3b08e980ec46d108fb508e4b345e90a847b
WesterAl <albin.westerlund@gmail.com>
2022-06-15 23:05:40 +0100

    test added local change and I will try to pull from main to see what will happen with the local change

--- app/src/main/java/com/example/swimtraining/MainActivity.java
+++ app/src/main/java/com/example/swimtraining/MainActivity.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
+    //local test
 
     //Initialize variable
     Chronometer chronometer;
