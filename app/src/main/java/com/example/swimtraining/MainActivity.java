280b907186a561a0371696d66197d88b66d907a7
WesterAl <67157428+WesterAl@users.noreply.github.com>
2022-06-15 17:14:16 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/swimtraining/MainActivity.java
+++ app/src/main/java/com/example/swimtraining/MainActivity.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
+    //test albin
 
     //Initialize variable
     Chronometer chronometer;
