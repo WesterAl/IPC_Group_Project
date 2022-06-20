a029d9ce3af15ca57207d71fb3b0616eef48db3c
letscia <eletronica.leticia@gmail.com>
2022-06-20 16:03:41 +0100

    click in the button alone student

--- app/src/main/java/com/example/swimtraining/Menu.java
+++ app/src/main/java/com/example/swimtraining/Menu.java
@@ -2,7 +2,12 @@ package com.example.swimtraining;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Menu extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button btn = (Button)findViewById(R.id.alone_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, TimerActivity.class));
+            }
+        });
     }
 }
